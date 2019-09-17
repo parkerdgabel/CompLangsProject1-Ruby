64b19db48790f534d91986daabe6a294a70a3676
UALGuest <ualguest@icmac611.local>
2019-09-17 15:30:22 -0700

    finished wordsort

--- wordsort.rb
+++ wordsort.rb
@@ -6,15 +6,22 @@ def wordify(infile)
   words
 end
 
-def binary_search(words, word, low = 0, high = nil)
-  high = words.count - 1 if high.nil?
+def binary_search(words, word, low = 0, high = words.length - 1)
+  if low == high
+    if words[low] > word
+      return low
+    else
+      return low + 1
+    end
+  end
+
+  return low if low > high
 
   mid = (low + high) / 2
-
-  if word < words[mid]
-    return binary_search words, word, low, mid - 1
-  elsif word > words[mid]
+  if words[mid] < word
     return binary_search words, word, mid + 1, high
+  elsif words[mid] > word
+    return binary_search words, word, low, mid - 1
   else
     return mid
   end
@@ -25,7 +32,7 @@ def wordsort(words)
   until i >= words.count
     j = i - 1
     word = words[i]
-    position = binary_search words, word, 0, j
+    position = binary_search words, word
     while j >= position
       words[j + 1] = words[j]
       j -= 1
